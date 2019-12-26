9cd397f037f85504a2b85ab19c7b3e414a5b4b12
robertohms <roberto@hmsconsulting.com.br>
2019-12-26 18:04:08 -0300

    Adicionado a quantidade de aniversáriantes dos mês

--- src/app/menu/pages/home/home.page.ts
+++ src/app/menu/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { UsuarioService } from 'src/app/core/services/usuario.service';
 import { ClienteService } from 'src/app/core/services/cliente.service';
@@ -14,6 +14,19 @@ import { OverlayService } from 'src/app/core/services/overlay.service';
 })
 export class HomePage implements OnInit {
   clientes$: Observable<Cliente[]>;
+
+  data: Date = new Date();
+  dia = this.data.getDate();
+  mes = this.data.getMonth() + 1;
+  ano = this.data.getFullYear();
+  // variaveis para saber os aniversáriantes do mês
+  dataAtual = [this.mes, this.dia].join('/');
+  mesAtual = parseInt([this.mes].join(''));
+  aniversariantesMes: Array<any> = [];
+  quantidadeAniversariantesMes: Array<any> = [];
+
+  @Input() cliente: Cliente;
+
   nomeUser = '...';
   urlFoto = '...';
   admin = false;
@@ -47,6 +60,11 @@ export class HomePage implements OnInit {
   linkDeclaracoes(){
     this.navCtrl.navigateForward("/menu/DeclaracaoAmbiental")
   }
+
+
+
+
+
   async ngOnInit() {
     this.nomeUser = this.usuario.nomeUser;
     this.urlFoto = this.usuario.urlFoto;
@@ -57,8 +75,62 @@ export class HomePage implements OnInit {
     this.clientes$ = this.clienteService.getAll();
     this.clientes$.pipe(take(1)).subscribe(() => loading.dismiss());
 
-
+    this.retornaDiasVencimento();
   }
 
+  async retornaDiasVencimento() {
 
+    this.clientes$.forEach(element => {
+
+      console.log('Cliente');
+      this.aniversariantesMes = [];
+      this.quantidadeAniversariantesMes = [];
+      let nomesAniversariantesDia: Array<any> = [];
+      let qtdNiverMes: any = 0;
+      let qtdArray: Array<any> = [];
+
+      element.forEach(cli => {
+        console.log(cli.dataNascimento);  
+        let dataInicialRecebida = new Date(cli.dataNascimento);
+        let dataFinalFormatada = (dataInicialRecebida.getMonth() + 1) + "/" + (dataInicialRecebida.getDate());
+        let dataInicialFormatada = this.dataAtual;
+
+        let dataMesAtualNiver = (dataInicialRecebida.getMonth() + 1);
+       // console.log('mes', dataMesAtualNiver, ' atual  ', this.mesAtual);
+
+
+        //console.log(dataInicialFormatada + " - " + dataFinalFormatada);
+
+        var dataInicialMilissegundos = new Date(dataInicialFormatada).getTime();
+        var dataFinalMilissegundos = new Date(dataFinalFormatada).getTime();
+
+        //console.log(dataInicialMilissegundos + ' - ' + dataFinalMilissegundos);
+
+        // Transforme 1 dia em milissegundos
+        var umDiaMilissegundos = 1000*60*60*24;
+
+        // Calcule a diferença em milissegundos
+        var diferencaMilissegundos = dataFinalMilissegundos  - dataInicialMilissegundos;
+
+        // Converta novamente para data
+        var diferencaData = Math.round(diferencaMilissegundos/umDiaMilissegundos);
+
+        console.log(diferencaData);
+
+        if (diferencaData == 0) {
+          nomesAniversariantesDia.push(cli.nome);
+        }
+
+        if (this.mesAtual == dataMesAtualNiver && diferencaData >= 0) {
+          console.log('54541541')
+          qtdNiverMes = qtdNiverMes + 1;
+          qtdArray.push(qtdNiverMes);
+        }
+      });
+
+      this.aniversariantesMes.push(nomesAniversariantesDia);
+      this.quantidadeAniversariantesMes = qtdArray;
+      console.log(this.aniversariantesMes, '------', this.quantidadeAniversariantesMes);
+    });
+  }
 }
