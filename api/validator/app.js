2f94d0b9b425762e2eaf2a8a0065aaa06490f276
SillasVinicius <sillasvinicius0403@gmail.com>
2020-01-21 16:10:32 -0300

    montagem do corpo email

--- api/validator/app.js
+++ api/validator/app.js
@@ -2,28 +2,230 @@ const config = require('./config');
 
 const db = config.db;
 
-let clientes = new Map();
-let outorga = [];
+let clientes;
+let outorgas;
+let declaracoesAmbientais;
+let licencasAmbientais;
+let cadastrosCredito;
 
-db.collection('cliente').get()
-    .then((snapshot) => {
-        snapshot.forEach((doc) => {
-            clientes.set(doc.id, doc.data());
-        });
-        // console.log(clientes);
-    })
-    .catch((err) => {
-        console.log('Error getting documents', err);
-    })
+let aniveriantesDia = [];
+let outorgasVencer30Dias;
+let outorgasVencer15Dias;
+let daVencer30Dias;
+let daVencer15Dias;
+let laVencer15Dias;
+let laVencer60Dias;
+let laVencer130Dias;
+let ccVencer15Dias;
+let ccVencer30Dias;
+let ccVencer130Dias;
 
-db.collection('outorga').get()
-    .then((snapshot) => {
-        snapshot.forEach((doc) => {
-            outorga.push(doc.data());
-        });
-        console.log(outorga);
-    })
-    .catch((err) => {
-        console.log('Error getting documents', err);
-    })
+// const dados = {
+//     clientes:[],
+//     outorgas:[],
+//     declaracoesAmbientais:[],
+//     licencasAmbientais:[],
+//     cadastrosCredito:[],
+//     aniveriantesDia:[],
+//     outorgasVencer30Dias:[],
+//     outorgasVencer15Dias:[],
+//     daVencer30Dias:[],
+//     daVencer15Dias:[],
+//     laVencer15Dias:[],
+//     laVencer60Dias:[],
+//     laVencer130Dias:[],
+//     ccVencer15Dias:[],
+//     ccVencer30Dias:[],
+//     ccVencer130Dias:[],
+// }
+
+async function buscaDados() {
+    // Limpa variaveis
+    clientes = [];
+    outorgas = [];
+    declaracoesAmbientais = [];
+    licencasAmbientais = [];
+    cadastrosCredito = [];
+
+    aniveriantesDia = [];
+    outorgasVencer30Dias = [];
+    outorgasVencer15Dias = [];
+    daVencer30Dias = [];
+    daVencer15Dias = [];
+    laVencer15Dias = [];
+    laVencer60Dias = [];
+    laVencer130Dias = [];
+    ccVencer15Dias = [];
+    ccVencer30Dias = [];
+    ccVencer130Dias = [];
+
+    // busca todos os clientes
+    await db.collection('cliente').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                clientes.push(doc.data());
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        })
+
+    // busca todas as outorgas
+    await db.collection('outorga').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                outorgas.push(doc.data());
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        })
+
+    // busca todas as declarações ambientais
+    await db.collection('DeclaracaoAmbiental').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                declaracoesAmbientais.push(doc.data());
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        })
+
+    // busca todas as licenças ambientais
+    await db.collection('LicencaAmbiental').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                licencasAmbientais.push(doc.data());
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        })
+
+    // busca todas as licenças ambientais
+    await db.collection('cadastroCredito').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                cadastrosCredito.push(doc.data());
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        })
+
+    // Busca os aniversariantes do dia
+    aniveriantesDia = clientes.filter(
+        (cliente) => {
+            dtNascimento = new Date(cliente.dataNascimento);
+            dtAniversario = `${dtNascimento.getDate()}-${dtNascimento.getMonth() + 1}`;
+            dtHojeCompleto = new Date();
+            dtHoje = `${dtHojeCompleto.getDate()}-${dtHojeCompleto.getMonth() + 1}`;
+            return dtAniversario === dtHoje ? true : false;
+        }
+    );
+
+    // Busca outorgas a vencer em 15 dias ou menos e 30 dias ou menos
+    outorgas.forEach(
+        (outorga) => {
+            dtVencimento = new Date(outorga.dataDeVencimento);
+            dtHoje = new Date();
+            diasDiferenca = Math.ceil((dtVencimento - dtHoje) / (1000 * 3600 * 24));
+            if (diasDiferenca <= 15) {
+                outorgasVencer15Dias.push(outorga);
+            } else if (diasDiferenca <= 30) {
+                outorgasVencer30Dias.push(outorga);
+            }
+        }
+    )
+
+    // Busca declaracoes ambientais a vencer em 15 dias ou menos e 30 dias ou menos
+    declaracoesAmbientais.forEach(
+        (da) => {
+            dtVencimento = new Date(da.dataDeVencimento);
+            dtHoje = new Date();
+            diasDiferenca = Math.ceil((dtVencimento - dtHoje) / (1000 * 3600 * 24));
+            if (diasDiferenca <= 15) {
+                daVencer15Dias.push(da);
+            } else if (diasDiferenca <= 30) {
+                daVencer30Dias.push(da);
+            }
+        }
+    )
+
+    // Busca declaracoes ambientais a vencer em 15 dias ou menos, 60 dias ou menos e 130 dias ou menos
+    licencasAmbientais.forEach(
+        (la) => {
+            dtVencimento = new Date(la.dataDeVencimento);
+            dtHoje = new Date();
+            diasDiferenca = Math.ceil((dtVencimento - dtHoje) / (1000 * 3600 * 24));
+            if (diasDiferenca <= 15) {
+                laVencer15Dias.push(la);
+            } else if (diasDiferenca <= 60) {
+                laVencer60Dias.push(la);
+            } else if (diasDiferenca <= 130) {
+                laVencer130Dias.push(la);
+            }
+        }
+    )
+
+    // Busca declaracoes ambientais a vencer em 15 dias ou menos, 30 dias ou menos e 130 dias ou menos
+    cadastrosCredito.forEach(
+        (cc) => {
+            dtExpiracao = new Date(cc.dataExpiracaoCredito);
+            dtHoje = new Date();
+            diasDiferenca = Math.ceil((dtExpiracao - dtHoje) / (1000 * 3600 * 24));
+            if (diasDiferenca <= 15) {
+                ccVencer15Dias.push(cc);
+            } else if (diasDiferenca <= 60) {
+                ccVencer30Dias.push(cc);
+            } else if (diasDiferenca <= 130) {
+                ccVencer130Dias.push(cc);
+            }
+        }
+    )
+
+    // tirar isso daqui
+    montarEmail();
+}
+
+// dividir em dois documentos, talvez usando um objeto como retorno
+buscaDados();
+function montarEmail() {
+    let htmlAniversariantes = '';
+    let htmlOutorgas = '';
+
+    // monta conteudo da seção de aniverasriantes do dia
+    if (aniveriantesDia) {
+        aniveriantesDia.forEach(
+            (aniversariante) => {
+                htmlAniversariantes += `<tr><td>${aniversariante.nome}<td></tr>`;
+            }
+        );
+    } else {
+        htmlAniversariantes += `<tr><td>Sem aniversariantes hoje!<td></tr>`;
+    }
+
+    if (outorgasVencer30Dias) {
+        outorgasVencer30Dias.forEach(
+            (outorgas) => {
+                
+            }
+        );
+    }
+
+    corpoEmail = 
+    `
+    <table style="width:100%">
+    <tr><th>Aniversariantes do dia</th></tr>
+    ${htmlAniversariantes}
+    <tr><th>Outorgas a vencer</th></tr>
+    <tr><th>30 dias:</th></tr>
+
+    <tr><th>15 dias:</th></tr>
+    </table>
+    `
+    console.log(corpoEmail);
+}
+
 
---
+++ api/validator/corpoEmail.js
@@ -0,0 +1,7 @@
+const estilo = `<style type="text/css">
+    .tg  {border-collapse:collapse;border-spacing:0;border-color:#bbb;}
+    .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-top-width:1px;border-bottom-width:1px;border-color:#bbb;color:#594F4F;background-color:#E0FFEB;}
+    .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-top-width:1px;border-bottom-width:1px;border-color:#bbb;color:#493F3F;background-color:#9DE0AD;}
+    .tg .tg-baqh{text-align:center;vertical-align:top}
+    .tg .tg-0lax{text-align:left;vertical-align:top}
+    </style>`;
